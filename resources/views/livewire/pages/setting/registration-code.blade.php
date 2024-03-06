505847022695e5ec4a39f62bfa8d3d9e44d2f563
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-03-06 12:40:14 +0800

    pembuatan layout

--- resources/views/livewire/pages/setting/registration-code.blade.php
+++ resources/views/livewire/pages/setting/registration-code.blade.php
@@ -1,3 +1,18 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="card w-full max-w-xl mx-auto bg-base-100 border-2">
+    <div class="card-body">
+        <h3 class="card-title">Kode registrasi</h3>
+        <div>Kode registrasi ini digunakan dalam proses pendaftaran peserta magang baru.</div>
+    </div>
+    <div class="grid place-content-center">
+        <div class="text-3xl font-bold font-mono py-12">9687IUHGI</div>
+    </div>
+    <div class="card-body">
+
+        <div class="card-actions">
+            <button class="btn btn-primary">
+                <x-tabler-reload class="icon-5" />
+                <span>Regenerate</span>
+            </button>
+        </div>
+    </div>
 </div>
