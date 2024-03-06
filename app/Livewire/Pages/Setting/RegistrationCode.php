fd25809dfdd94d75386a5d8a1e925191e9d98ca3
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-03-06 12:56:07 +0800

    buat algoritma untuk random kode

--- app/Livewire/Pages/Setting/RegistrationCode.php
+++ app/Livewire/Pages/Setting/RegistrationCode.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\Pages\Setting;
 
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class RegistrationCode extends Component
 {
+    public $kode;
+
+    public function generateKode(){
+        $this->kode = implode('', [
+            Str::substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 3),
+            rand(100, 999)
+        ]);
+    }
+
+    public function mount(){
+        $this->generateKode();
+    }
+
     public function render()
     {
         return view('livewire.pages.setting.registration-code');
--- resources/views/livewire/pages/setting/registration-code.blade.php
+++ resources/views/livewire/pages/setting/registration-code.blade.php
@@ -1,15 +1,13 @@
-<div class="card w-full max-w-xl mx-auto bg-base-100 border-2">
-    <div class="card-body">
+<div class="card w-full max-w-md mx-auto bg-base-100 border-2">
+    <div class="card-body space-y-6">
         <h3 class="card-title">Kode registrasi</h3>
-        <div>Kode registrasi ini digunakan dalam proses pendaftaran peserta magang baru.</div>
-    </div>
-    <div class="grid place-content-center">
-        <div class="text-3xl font-bold font-mono py-12">9687IUHGI</div>
-    </div>
-    <div class="card-body">
-
+        <p>Kode registrasi ini digunakan dalam proses pendaftaran peserta magang baru.</p>
+        <div class="text-3xl font-bold font-mono bg-base-200 rounded-2xl p-12 text-center border-2 border-base-300">
+            {{ $kode }}
+        </div>
+        <div class="divider"></div>
         <div class="card-actions">
-            <button class="btn btn-primary">
+            <button class="btn btn-primary" wire:click="generateKode">
                 <x-tabler-reload class="icon-5" />
                 <span>Regenerate</span>
             </button>
