02919f9d8bea684b29d024ca70a862b07056a5ee
rigel-sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-10 14:24:55 +0700

    Update mine.blade.php
    Tampil data riwayat absensi

--- resources/views/livewire/pages/attendance/mine.blade.php
+++ resources/views/livewire/pages/attendance/mine.blade.php
@@ -1,3 +1,32 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div>
+        <h1>Riwayat Absensi</h1>
+        <div class="flex justify-end mb-3">
+   
+            <input type="date" class="form-control" wire:model="searchDate" placeholder="Cari">
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Tanggal</th>
+                    <th>Absen Masuk</th>
+                    <th>Absen Keluar</th>                  
+                    <th>Catatan</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($mine as $index => $data)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $data->date }}</td>
+                        <td>{{ $data->in }}</td>
+                        <td>{{ $data->out }}</td>
+                        <td>{{ $data->note }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
