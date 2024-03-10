fc409d6b094913cc2c2bf2af3e18c1d7768103c9
rigel-sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-10 14:25:59 +0700

    Update Mine.php
    Membuat controller untuk menampilkan data riwayat absensi

--- app/Livewire/Pages/Attendance/Mine.php
+++ app/Livewire/Pages/Attendance/Mine.php
@@ -3,11 +3,25 @@
 namespace App\Livewire\Pages\Attendance;
 
 use Livewire\Component;
+use App\Models\Attendance; 
+use RealRashid\SweetAlert\Facades\Alert;
 
 class Mine extends Component
 {
+    public $searchDate;
+    public $mine;
+
+  
     public function render()
     {
-        return view('livewire.pages.attendance.mine');
+        // Melakukan pencarian data absensi untuk pengguna yang sedang login berdasarkan tanggal
+        $this->mine = Attendance::where('user_id', auth()->user()->id)
+                                ->get();
+
+        return view('livewire.pages.attendance.mine', [
+            'mine' => $this->mine
+        ]);
     }
+
+
 }
