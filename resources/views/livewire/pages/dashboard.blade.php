eda99ccb82dfd7158e3879cb34dcb8bc22522622
iqbal farhan syuhada <iqbalfarhan1996@gmail.com>
2024-03-13 02:00:27 +0800

    buat gretting dashboard

--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="space-y-6">
+    <div class="alert border-2">
+        <x-tabler-user class="stroke-info" />
+        <div>
+            <h3 class="font-bold">Halo selamat datang</h3>
+            <div class="text-xs">You have 1 unread message</div>
+        </div>
+        <button class="btn btn-sm">See</button>
+    </div>
 </div>
