6a0b313ab056c000284d3de95dda49d99c0be0aa
rigel-sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-14 15:33:34 +0700

    Memperbaiki format , pencarian tanggal,  dan menambahkan status approved

--- resources/views/livewire/pages/attendance/mine.blade.php
+++ resources/views/livewire/pages/attendance/mine.blade.php
@@ -1,9 +1,7 @@
 <div>
     <div>
-        <h1>Riwayat Absensi</h1>
-        <div class="flex justify-end mb-3">
-   
-            <input type="date" class="form-control" wire:model="searchDate" placeholder="Cari">
+        <div class="flex justify-end mb-3">  
+            <input type="date" class="input input-bordered" wire:model.live="date" />
         </div>
         <div class="table-wrapper">
         <table class="table">
@@ -21,10 +19,17 @@
                 @foreach ($mine as $index => $data)
                     <tr>
                         <td>{{ $index + 1 }}</td>
-                        <td>{{ $data->date }}</td>
-                        <td>{{ $data->in }}</td>
-                        <td>{{ $data->out }}</td>
+                        <td>{{ $data->date->format('d F Y') }}</td>
+                        <td>{{ $data->in->format('H:i') }}</td>
+                        <td>{{ $data->out ? $data->out->format('H:i') : '-' }}</td>
                         <td>{{ $data->note }}</td>
+                        <td>
+                            @if ($data->approved == 1)
+                                Disetujui
+                            @else
+                                -
+                            @endif
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
