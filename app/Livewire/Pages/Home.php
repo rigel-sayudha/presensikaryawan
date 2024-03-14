2f42fca799865eeec8f1f920ccbbba2011fa82cd
rigel-sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-14 16:38:13 +0700

    Menambahkan fungsi mount

--- app/Livewire/Pages/Home.php
+++ app/Livewire/Pages/Home.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Home extends Component
 {
+     public function mount()
+    {    
+        $this->user = Auth::user();
+    }
     public function render()
     {
         return view('livewire.pages.home');
