84fa35f2cea1d7fd4b2b337ff3469537d9964a04
rigel-sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-14 16:39:02 +0700

    Update Home.php

--- app/Livewire/Pages/Home.php
+++ app/Livewire/Pages/Home.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Livewire\Pages;
-
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Models\Attendance;
+use Illuminate\Support\Facades\Session;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use App\Livewire\Forms\UserForm;
 use Livewire\Component;
 
 class Home extends Component
 {
+    public $user; 
      public function mount()
     {    
         $this->user = Auth::user();
