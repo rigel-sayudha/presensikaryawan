1a9f94d657743a90ef4739738a79d9a611c41c40
iqbal farhan syuhada <iqbalfarhan1996@gmail.com>
2024-03-17 03:51:54 +0800

    add count attendance to dashboard

--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -3,6 +3,7 @@
 
     <div class="grid grid-cols-3 gap-6">
         @livewire('widget.check-attendance')
+        @livewire('widget.count-attendance')
     </div>
 
     @livewire('pages.attendance.show')
