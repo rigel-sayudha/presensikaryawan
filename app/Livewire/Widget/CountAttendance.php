bbf3e9b103e0d16693d0fdac2cf3579e1a7888f3
iqbal farhan syuhada <iqbalfarhan1996@gmail.com>
2024-03-17 03:45:06 +0800

    buat file count attendance

---
+++ app/Livewire/Widget/CountAttendance.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Widget;
+
+use Livewire\Component;
+
+class CountAttendance extends Component
+{
+    public function render()
+    {
+        return view('livewire.widget.count-attendance');
+    }
+}
