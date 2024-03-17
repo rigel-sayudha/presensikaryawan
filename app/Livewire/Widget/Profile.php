4e8b11e237d44b510f935f8ae9c1e726ee228e2b
Rigel Sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-17 12:44:04 +0700

    Update Profile.php

--- app/Livewire/Widget/Profile.php
+++ app/Livewire/Widget/Profile.php
@@ -1,1 +1,16 @@
+<?php
+
+namespace App\Livewire\Widget;
+
+use Livewire\Component;
+
+class Profile extends Component
+{
+    public function render()
+    {
+        return view('livewire.widget.profile', [
+            'user' => auth()->user()
+        ]);
+    }
+}
 
