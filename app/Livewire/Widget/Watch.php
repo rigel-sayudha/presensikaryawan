1e45320b1b098ab63314dd52dd438971955e504c
Rigel Sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-17 12:44:29 +0700

    Update Watch.php

--- app/Livewire/Widget/Watch.php
+++ app/Livewire/Widget/Watch.php
@@ -1,1 +1,20 @@
+<?php
+
+namespace App\Livewire\Widget;
+
+use Livewire\Component;
+
+class Watch extends Component
+{
+    public $currentTime;
+
+    public function mount()
+    {
+        $this->currentTime = now()->toTimeString();
+    }
+    public function render()
+    {
+        return view('livewire.widget.watch');
+    }
+}
 
