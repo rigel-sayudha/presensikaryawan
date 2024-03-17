20540f5062da5e77491d070eb5eef98b68b86b75
Rigel Sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-17 12:44:53 +0700

    Update Absensi.php

--- app/Livewire/Widget/Absensi.php
+++ app/Livewire/Widget/Absensi.php
@@ -1,1 +1,14 @@
+<?php
+
+namespace App\Livewire\Widget;
+
+use Livewire\Component;
+
+class Absensi extends Component
+{
+    public function render()
+    {
+        return view('livewire.widget.absensi');
+    }
+}
 
