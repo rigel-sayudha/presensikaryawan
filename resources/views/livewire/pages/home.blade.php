2ac5da7e2ec8e61d0c4e7351c6a8fd68fe7cfc24
Rigel Sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-17 13:11:22 +0700

    menambahkan widget profil, absensi, jam

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -1,25 +1,7 @@
-<div>
-<p>Pukul: {{ now()->format('H:i') }}</p>
-<br>
-    <div class="card card-compact bg-base-100 border-2">
-            <div class="card-body space-y-4 border-b">
-                <div class="flex flex-col md:flex-row gap-6">
-                    <div class="flex items-center justify-center md:aspect-square">
-                        <div class="avatar">
-                            <div class="w-28 rounded-lg">
-                                <img src="{{ $user->avatar }}" alt="">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="grid md:grid-cols-2 xl:grid-cols-3 flex-1">
-                        @foreach (['name', 'email', 'school', 'phone', 'nis'] as $key)
-                            <div class="flex flex-col p-2 hover:bg-base-200">
-                                <span class="text-xs capitalize opacity-50">{{ $key }}</span>
-                                <div>{{ $user->$key }}</div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
+<div class="space-y-6">
+    <div class="grid grid-cols-3 gap-6">
+        @livewire('widget.check-attendance')
+        @livewire('widget.watch')
+        @livewire('widget.profile')
     </div>
 </div>
