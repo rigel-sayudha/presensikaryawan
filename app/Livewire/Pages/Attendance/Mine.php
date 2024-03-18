87148ea77c7e915d73e42b2d02e9c384d09acb64
Rigel Sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-18 19:15:23 +0700

    Menambahkan script

--- app/Livewire/Pages/Attendance/Mine.php
+++ app/Livewire/Pages/Attendance/Mine.php
@@ -10,18 +10,26 @@ class Mine extends Component
 {
     public $searchDate;
     public $mine;
-
+    public $no = 1;
+    public $date;
+    public $search;
   
     public function render()
     {
-        // Melakukan pencarian data absensi untuk pengguna yang sedang login berdasarkan tanggal
-        $this->mine = Attendance::where('user_id', auth()->user()->id)
-                                ->get();
+          $this->mine = Attendance::where('user_id', auth()->user()->id)
+                        ->when($this->search, function($q) {
+                            $q->whereHas('user', function($w) {
+                                $w->where('name', 'like', "%{$this->search}%");
+                            });
+                        })
+                        ->when($this->date, function($q) {
+                            $q->whereDate('date', $this->date);
+                        })
+                        ->orderBy('date')
+                        ->get();
 
         return view('livewire.pages.attendance.mine', [
             'mine' => $this->mine
         ]);
     }
-
-
 }
