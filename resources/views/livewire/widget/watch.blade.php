2e6a0a339953cca15f142722a90c824486666a8d
Rigel Sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-18 13:31:51 +0700

    memperbaiki waktu dan tanggal

--- resources/views/livewire/widget/watch.blade.php
+++ resources/views/livewire/widget/watch.blade.php
@@ -1,12 +1,12 @@
 <div class="card card-compact bg-base-100 border-2">
     <div class="card-body flex flex-col items-center justify-center h-full">
         <div class="text-center text-4xl font-bold" 
-            x-data="{ time: '{{ $currentTime }}' }" 
+            x-data="{ time: '{{ $waktu }}' }" 
             x-init="setInterval(() => {
                 time = new Date().toLocaleTimeString();
             }, 1000)">
             <span x-text="time"></span>
-            <div class="text-xs">{{ date('d F Y', strtotime($currentDate)) }}</div>
+            <div class="text-xs">{{ date('d F Y', strtotime($tanggal)) }}</div>
         </div>
     </div>
 </div>
