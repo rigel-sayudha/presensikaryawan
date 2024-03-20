78753d8be2454a070f58f50827a6e605ffe8eed5
IQBAL FARHAN SYUHADA <iqbalfarhan1996@gmail.com>
2024-03-20 22:11:38 +0800

    perbaikan halaman login tambah error directive

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,10 +1,11 @@
 <form class="card-body space-y-3" wire:submit="login">
     <div class="flex flex-col gap-2">
-        <label for="" class="input input-bordered flex items-center gap-2">
-            <x-tabler-user class="icon-5" />
-            <input type="text" class="grow" placeholder="Email address" wire:model="email">
+        <label for="" class="input input-bordered flex items-center gap-2 @error('email') input-error @enderror">
+            <x-tabler-at class="icon-5" />
+            <input type="email" class="grow" placeholder="Email address" wire:model="email" autocomplete="email">
         </label>
-        <label for="" class="input input-bordered flex items-center gap-2">
+        <label for=""
+            class="input input-bordered flex items-center gap-2 @error('password') input-error @enderror">
             <x-tabler-key class="icon-5" />
             <input type="password" class="grow" placeholder="Password" wire:model="password">
         </label>
