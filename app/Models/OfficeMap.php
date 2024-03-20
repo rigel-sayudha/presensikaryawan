13d88e6a58f620c9a9cc39afc2079f3a9b92893b
IQBAL FARHAN SYUHADA <iqbalfarhan1996@gmail.com>
2024-03-20 21:24:57 +0800

    update model Office Map

--- app/Models/OfficeMap.php
+++ app/Models/OfficeMap.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class OfficeMap extends Model
 {
     use HasFactory;
 
-    protected $listeners = [
+    protected $fillable = [
         'name',
         'building',
         'floor',
         'desc',
         'photo',
     ];
+
+    public function getImageAttribute(){
+        return $this->photo ? Storage::url($this->photo) : url('nouser.jpg');
+    }
 }
