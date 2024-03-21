6dd7ea5cd09da9469fc14014e2f3afb21e01bdf2
Rigel Sayudha <61726660+rigel-sayudha@users.noreply.github.com>
2024-03-21 22:15:14 +0700

    Membuat list peta gedung

--- resources/views/livewire/pages/office/search.blade.php
+++ resources/views/livewire/pages/office/search.blade.php
@@ -1,4 +1,5 @@
-<div>
+<div class="relative">
+  @livewire('pages.office.item')
     {{-- list item office muncul hanya ketika pencarian di submit --}}
     {{-- isi data dummy office menggunakan perintah "php artisan db:seed OfficeMapSeeder" --}}
 </div>
