e51ec831f4e1a5b649eedafe9f73d6eef87d19f6
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-03-22 09:21:24 +0800

    membuat livewire form officemap

---
+++ app/Livewire/Forms/OfficeMapForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use App\Models\OfficeMap;
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class OfficeMapForm extends Form
+{
+    public $name;
+    public $building;
+    public $floor;
+    public $desc;
+    public $photo;
+
+    public OfficeMap $officeMap;
+
+    public function setOfficeMap(OfficeMap $officeMap){
+        $this->officeMap = $officeMap;
+
+        $this->name = $officeMap->name;
+        $this->building = $officeMap->building;
+        $this->floor = $officeMap->floor;
+        $this->desc = $officeMap->desc;
+        $this->photo = $officeMap->photo;
+    }
+
+    public function store(){
+        $valid = $this->validate([
+            'name' =>'required',
+            'building' =>'required',
+            'floor' =>'required',
+            'desc' =>'required',
+            'photo' =>'required',
+        ]);
+
+        OfficeMap::create($valid);
+        $this->reset();
+    }
+
+    public function update(){
+        $valid = $this->validate([
+            'name' =>'required',
+            'building' =>'required',
+            'floor' =>'required',
+            'desc' =>'required',
+            'photo' =>'required',
+        ]);
+
+        $this->officeMap->update($valid);
+        $this->reset();
+    }
+}
